ffc4d3bf53e7cb97cea88587b6ee6e03104ea383
mohamed-el-sharkawy <melsharkawy346@gamil.com>
2022-03-19 22:19:40 +0200

    update post route handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express from 'express';
-import { helloRouter } from './routes/hello';
+import express from "express";
+import { helloRouter } from "./routes/hello";
+import { UpdatePostRouter } from "./routes/update_post";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(UpdatePostRouter);
 app.use(helloRouter);
 app.listen(3000, () => {
-    console.log('Server running at localhost:3000');
+  console.log("Server running at localhost:3000");
 });
---
+++ src/routes/update_post.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { posts } from "../posts";
+
+const router = Router();
+
+router.put("/posts/:postId", (req, res) => {
+  const content = req.body.content;
+  const postId = req.params.postId;
+  const id = parseInt(postId);
+  const found = posts.find((element) => element.id === id);
+  if (!found) {
+    res.status(404).send("the post is not found");
+  }
+  found!.content = content || found!.content;
+  res.status(201).send(content);
+});
+
+export { router as UpdatePostRouter };
