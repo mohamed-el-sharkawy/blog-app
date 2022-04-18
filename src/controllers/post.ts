c92850ce4e40769d6ec7bd7f8c342f5afd4719e8
muhammadmohie <muhammadmohie98@gmail.com>
2022-04-18 17:21:37 +0200

    Adding delete functions to the controller

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from "express";
-import { Post } from "../models/post";
-import mongoose from "mongoose";
+import { Request, Response } from 'express';
+import { Post } from '../models/post';
+import mongoose from 'mongoose';
 
 const createPost = async (req: Request, res: Response) => {
   const content = req.body.content;
-  if (!content) return res.status(400).json({ message: "Must send content" });
+  if (!content) return res.status(400).json({ message: 'Must send content' });
   const post = new Post({ content });
   await post.save();
   res.status(201).send(post);
@@ -15,13 +15,13 @@ const updatePost = async (req: Request, res: Response) => {
   const postId = req.params.postId;
 
   if (!mongoose.Types.ObjectId.isValid(postId)) {
-    return res.status(400).send("Invalid post id");
+    return res.status(400).send('Invalid post id');
   }
 
   const post = await Post.findById(postId);
 
   if (!post) {
-    return res.status(404).send("The post is not found");
+    return res.status(404).send('The post is not found');
   }
   post.content = content || post.content;
   await post.save();
@@ -29,4 +29,26 @@ const updatePost = async (req: Request, res: Response) => {
   return res.status(201).send(post);
 };
 
-export { createPost, updatePost };
+const deletePost = async (req: Request, res: Response) => {
+  // Check if the post already exists
+  const { postId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    return res.status(400).send('Invalid post id');
+  }
+
+  const post = await Post.findById(postId);
+  if (!post) return res.status(404).send("The requested post doesn't exist.");
+
+  // delete the requested post
+  await post.delete();
+  res.status(200).send(post);
+};
+
+const deletePosts = async (req: Request, res: Response) => {
+  await Post.deleteMany({});
+  // delete all posts
+  res.status(204).send();
+};
+
+export { createPost, updatePost, deletePost, deletePosts };
