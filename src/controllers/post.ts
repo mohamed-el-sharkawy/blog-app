ec42472faa227d8ffef031767175bb9672ae50c7
Adham Salama <adhamsalama@zohomail.com>
2022-04-18 16:51:59 +0200

    Add createPost to post controller

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -1,2 +1,12 @@
 import { Request, Response } from 'express';
 import { Post } from '../models/post';
+
+const createPost = async (req: Request, res: Response) => {
+    const content = req.body.content;
+    if (!content) return res.status(400).json({ message: 'Must send content' });
+    const post = new Post({ content });
+    await post.save();
+    res.status(201).send(post);
+};
+
+export { createPost };
