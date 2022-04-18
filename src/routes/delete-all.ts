ce4e963ddf40e6475ee0c523cfe00a51a00547e4
muhammadmohie <muhammadmohie98@gmail.com>
2022-04-18 16:40:02 +0200

    Update delete route handlers

--- src/routes/delete-all.ts
+++ src/routes/delete-all.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import { Post } from '../models/post';
-// import { posts } from '../posts';
 
 const router = Router();
 
 router.delete('/api/posts', async (req, res) => {
   await Post.deleteMany({});
   // delete all posts
-  res.status(204);
+  res.status(204).send();
 });
 
 export { router as deleteAllRouter };
