4b58db182f73b4e367e1d328a671e364895a8d91
mohamed-el-sharkawy <=>
2022-04-18 17:44:36 +0200

    remove not needed routers

--- src/routes/get-all.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { Post } from "../models/post";
-import { getPosts } from "../controllers/post";
-const router = Router();
-router.get("/api/posts", getPosts);
-
-export { router as getPostsRouter };
--- src/routes/get.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { Post } from "../models/post";
-import mongoose from "mongoose";
-import { getPost } from "../controllers/post";
-const router = Router();
-router.get("/api/posts/:id", getPost);
-
-export { router as getPostRouter };
