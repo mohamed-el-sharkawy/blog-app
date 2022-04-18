0aa7acb10002439edeb97d493ddea213f9094643
mohamed-el-sharkawy <=>
2022-04-18 17:45:02 +0200

    use controllers

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
-import { Post } from '../models/post';
+import {
+    createPost,
+    deletePost,
+    deletePosts,
+    getPost,
+    getPosts,
+    updatePost,
+} from '../controllers/post';
 const router = Router();
 
-router.post('/api/posts', async (req, res) => {
-    const content = req.body.content;
-    if (!content) return res.status(400).json({ message: 'Must send content' });
-    const post = new Post({ content });
-    await post.save();
-    res.status(201).send(post);
-});
+router.post('/api/posts', createPost);
+router.put('/api/posts/:postId', updatePost);
+router.get('/api/posts/:id', getPost);
+router.get('/api/posts', getPosts);
+router.delete('/api/posts', deletePosts);
+router.delete('/api/posts/:postId', deletePost);
 
 export { router as postRouter };
