db890181f4d7040b311d110a1a66f7ecb2cd43a9
mohamed-el-sharkawy <melsharkawy346@gmail.com>
2022-04-19 21:23:26 +0200

    update comment router

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { createComment } from '../controllers/comment';
+import { createComment, updateComment } from '../controllers/comment';
 
 const router = Router();
 
 router.post('/api/posts/:postId/comments', createComment);
+router.put('/api/posts/:postId/comments/:commentId', updateComment);
 
 export { router as commentRouter };
