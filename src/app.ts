7862d661824fe36e42ecf289df0b367a4a1449b6
Adham Salama <adhamsalama@zohomail.com>
2022-04-19 19:50:53 +0200

    Use commentController

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { postRouter } from './routes/post';
+import { commentRouter } from './routes/comment';
 import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.json());
 app.use(postRouter);
+app.use(commentRouter);
 
 async function run() {
     await mongoose.connect('mongodb://localhost:27017');
