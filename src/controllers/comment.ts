d3ebf36971a7c3455f6c03e420a5c8f8c48d7a43
mohamed-el-sharkawy <melsharkawy346@gmail.com>
2022-04-19 21:23:08 +0200

    add updateComment function

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 import { Comment } from '../models/comment';
 
 const createComment = async (req: Request, res: Response) => {
@@ -10,4 +11,25 @@ const createComment = async (req: Request, res: Response) => {
     res.status(201).send(comment);
 };
 
-export { createComment };
+const updateComment = async (req: Request, res: Response) => {
+    const { postId, commentId } = req.params;
+    const content = req.body.content;
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        return res.status(400).send('Invalid post id');
+    }
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
+        return res.status(400).send('Invalid comment id');
+    }
+    const comment = await Comment.findById(commentId);
+
+    if (comment.postId.toString() !== postId) {
+        return res.status(400).send('Invalid post');
+    }
+
+    comment.content = content || comment.content;
+    await comment.save();
+
+    res.status(201).send(comment);
+};
+
+export { createComment, updateComment };
