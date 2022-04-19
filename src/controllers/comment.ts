81a812ed5ee980a5fc06a0354f64a6841c6c818f
Adham Salama <adhamsalama@zohomail.com>
2022-04-19 19:50:00 +0200

    Add createComment

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -1,4 +1,13 @@
 import { Request, Response } from 'express';
 import { Comment } from '../models/comment';
 
-export {};
+const createComment = async (req: Request, res: Response) => {
+    const postId = req.params.postId;
+    const { content } = req.body;
+    console.log({ content });
+    const comment = new Comment({ content, postId });
+    await comment.save();
+    res.status(201).send(comment);
+};
+
+export { createComment };
