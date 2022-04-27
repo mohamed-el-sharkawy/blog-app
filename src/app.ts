a17f4d4420137405221952296f98006d1e2c1493
mohamed-el-sharkawy <melsharkawy346@gmail.com>
2022-04-27 02:30:09 +0200

    add auth router

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,20 @@ import { postRouter } from './routes/post';
 import { commentRouter } from './routes/comment';
 import mongoose from 'mongoose';
 import 'dotenv/config';
+import cookieSession from 'cookie-session';
+import { AuthRouter } from './routes/auth';
 
 const app = express();
 
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+);
+
 app.use(express.json());
+app.use(AuthRouter);
 app.use(postRouter);
 app.use(commentRouter);
 
